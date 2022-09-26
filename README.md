b5218035638a3eda2f5f2a09409d2f795c4e320d
Lucas Matos <66981887+LucasMatos86@users.noreply.github.com>
2022-09-26 18:08:35 -0300

    NodeJS-webSocket

---
+++ README.md
@@ -0,0 +1,10 @@
+# NodeJs + WebSocket
+Projeto desenvolvido na disciplina Redes de Computadores II.
+
+Professor: Rafael Roque.
+
+## Equipe
+- [Lucas Matos](https://github.com/LucasMatos86)
+- [Pedro Silva](https://github.com/PedroSilvaBarros)
+
+## Assuntos Abordados 
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static("public"))
+
+const http = require('http').Server(app)
+
+const serverSocket = require('socket.io')(http)
+
+const PORTA = process.env.PORT || 8001
+
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+
+http.listen(PORTA, () => {
+    const portaStr = PORTA === 80 ? '' :  ':' + PORTA
+    if (process.env.HEROKU_APP_NAME)
+        console.log('Servidor iniciado. Abra o navegador em ' + host)
+    else console.log('Servidor iniciado. Abra o navegador em ' + host + portaStr)
+})
+
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'))
+
+serverSocket.on('connect', recebeConexaoUsuario)
+
+function recebeConexaoUsuario(socket) {
+    socket.on('login', (nickname) => registraLoginUsuario(socket, nickname))
+    socket.on('disconnect', () => console.log('Cliente desconectado: ' + socket.nickname))
+    socket.on('chat msg', (msg) => encaminhaMsgsUsuarios(socket, msg))
+    socket.on('status', (msg) => encaminhaMsgStatus(socket, msg))
+}
+
+function encaminhaMsgStatus(socket, msg) {
+    console.log(msg)
+    socket.broadcast.emit('status', msg)
+}
+
+function encaminhaMsgsUsuarios(socket, msg) {
+    if(msg ===''){
+        return
+    }
+    serverSocket.emit('chat msg', `${socket.nickname} diz: ${msg}`)
+}
+
+function registraLoginUsuario(socket, nickname) {
+    if(nickname ===''){
+        return
+    }
+    socket.nickname = nickname
+    const msg = nickname + ' conectou'
+    console.log(msg)
+    serverSocket.emit('chat msg', msg)
+    
+}
